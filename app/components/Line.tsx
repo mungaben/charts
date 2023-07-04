78150da5f8f8dd3042304652482bce748fa75004
mungaben <kamaumungai3742@gmail.com>
2023-07-04 13:45:23 +0300

    chart in reatcahrts

---
+++ app/components/Line.tsx
@@ -0,0 +1,61 @@
+"use client";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+import React from "react";
+import { faker } from "@faker-js/faker";
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+  ],
+};
+const LineChart = () => {
+  return (
+    <div>dashed
+      <Line options={options} data={data} />;
+    </div>
+  );
+};
+
+export default LineChart;
